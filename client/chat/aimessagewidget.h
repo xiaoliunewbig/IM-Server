7dfdf3fa8466d8edac313a50a8fd30f21b32e5d3
ChengKang Peng <2801164069@qq.com>
2024-12-17 21:59:27 +0800

    client提交

---
+++ client/chat/aimessagewidget.h
@@ -0,0 +1,25 @@
+﻿#ifndef AIMESSAGEWIDGET_H
+#define AIMESSAGEWIDGET_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QHBoxLayout>
+
+class AiMessageWidget :public QWidget
+{
+    Q_OBJECT
+public:
+    explicit AiMessageWidget(const QString& avatarPath, const QString& message, bool isUser, QWidget *parent = nullptr);
+    void updateMessage(const QString& newMessage);
+
+    // 重写 sizeHint 方法
+    QSize sizeHint() const override;
+
+private:
+    QLabel* avatarLabel;    // 头像标签
+    QLabel* messageLabel;   // 消息内容标签
+    QHBoxLayout* layout;    // 布局
+    bool isUserMessage;     // 是否为用户消息
+};
+
+#endif // AIMESSAGEWIDGET_H
